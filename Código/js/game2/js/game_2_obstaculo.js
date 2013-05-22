626b0759aca97ebe23504abcddfec1bbeab13f11
raquelmelorj8@gmail.com <raquelmelorj8@gmail.com@59fae9b3-b18f-dced-0e73-08d28e2dbc6d>
2013-05-22 22:54:52 +0000

    Pastas organizadas.

---
+++ Código/js/game2/js/game_2_obstaculo.js
@@ -0,0 +1,47 @@
+function ObstaculoGame2(arquivo, tamanho_w, tamanho_h, pontos, vida)
+{
+	this.imagem = new Image();
+	this.imagem.src = arquivo;
+	this.carregar = false;
+	this.visivel = true;
+	
+	this.imagem.onload = function()
+	{
+		carregar = true; 
+		console.log("Uhuul Pessoas u.u" + carregar);
+	};
+	
+	this.tamanho_w = tamanho_w;
+    this.tamanho_h = tamanho_h;
+    this.posicao_x = Math.floor((Math.random()*(SCREENWIDTH-this.tamanho_w))); 
+    this.posicao_y = 0;
+    this.velocidade_x = 0;
+    this.velocidade_y = Math.floor((Math.random()*10)+5);
+    
+    this.velocidade_MAX = 15;
+    
+    this.pontos = pontos;
+    
+    this.vida = vida;
+    
+    this.update=function()
+    {
+    	console.log("update pessoas");
+    	
+    	this.posicao_x += this.velocidade_x;	
+
+    	this.posicao_y += this.velocidade_y;	
+           		
+    };
+    
+    this.reset=function()
+    {   
+    	console.log("reset pessoas");
+
+		this.posicao_y = -this.tamanho_h;
+		this.posicao_x = Math.floor((Math.random()*(SCREENWIDTH-this.tamanho_w))); 
+		this.velocidade_y = Math.floor((Math.random()*10)+5);
+    	
+    };
+ 
+ }
---
+++ Código/js/game4/js/game_4_scene_level_1.js
@@ -0,0 +1,99 @@
+function Game4SceneLevel4()
+{//abre Intro
+		this.reset=function()
+	    {
+        //file, size_x, size_y, pos_x, pos_y
+		this.fundo = new Fundo("imgs/game_4/fundo1.png", 600, 800, 0, 0);
+		//this.toy = new Player ("imgs/game_3/spencer_boy.png", 60, 70, ((SCREENWIDTH-94)/2)-40, 0);
+		//this.fundo = new Fundo_move ("imgs/game_3/fundo_move.png",1200, 800, 0, 0);
+		this.nave1 = new Player4("imgs/game_4/nave.png", 60, 40, 10, 200);
+		//file, size_x, size_y, pos_x, pos_y, vel_x, vel_y
+		this.ast = new Asteroide("imgs/game_4/asteroid.png", 40, 32,400,300,-2,2);
+		this.button_back = new MyButton("imgs/close.png", 50, 50, 700, 50);
+		
+		}//fim reset
+		
+		this.reset();
+		
+    this.update=function()
+    {
+		
+        this.nave1.update();
+		this.ast.update();
+		
+		
+		
+    };
+    
+    this.draw=function()
+    {
+		this.fundo.draw();
+		this.nave1.draw();
+		this.ast.draw();
+		this.button_back.draw();
+		
+
+		
+        screen.font = "20px Comic Sans MS";
+        screen.fillStyle="#000000";
+		screen.fillText("Pontos:", 20, 20);
+                
+				
+        //this.button_credits.draw();
+
+        //this.button_start.draw();
+		
+		//this.button_instru.draw();
+				          
+    }
+    
+    this.mouse_down=function(mouse)
+    {//abre down
+      		if(this.button_back.clicked(mouse)) //botão para voltar o menu
+        {
+        	currentScene = SCENE.MENU;
+        	
+        	game4.currentGameScene = game4.GAMESCENE.INTRO;
+        	 
+        	//this.player.points = 0;
+        
+        	this.reset();
+        }  
+			
+		
+    }//fecha down
+    
+    this.mouse_up=function(mouse)
+    {
+                //
+        
+    }
+	
+		this.mouse_move=function(mouse)
+  	{
+  		//console.log("Game4 mouse X " + mouse.x + " mouse Y " + mouse.y );
+  		
+  		if(this.button_back.mouse_over(mouse))
+  		{
+  			
+  			this.button_back.current_frame = 0;
+  		}
+  		else
+  		{
+  			this.button_back.current_frame = 1;
+  		}
+		
+	}
+    
+    this.key_down=function(key)
+    {
+       this.nave1.key_down(key)
+    }
+    
+    this.key_up=function(key)
+    {
+        this.nave1.key_up(key)
+    }
+
+    
+}//fecha Intro
