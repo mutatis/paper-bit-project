185707b711bc9fafcfc1730276e87310531d4cc1
raquelmelorj8@gmail.com <raquelmelorj8@gmail.com@59fae9b3-b18f-dced-0e73-08d28e2dbc6d>
2013-05-26 03:57:19 +0000

    Imagens.

--- Código/js/game2/js/fundo_paisagem.js
+++ Código/js/game2/js/fundo_paisagem.js
@@ -1,29 +1,38 @@
  function Fundo_paisagem(arquivo, posicao_x, posicao_y)
-{
-
-	this.imagem = new Image();
-	this.imagem.src = arquivo;
-	this.carregar = false;
-	this.visivel = true;
-
-	this.imagem.onload = function()
+{//abre fundo
+	
+	this.img = new Image();
+	this.img.src = arquivo;
+	this.loaded = false;
+	this.visible = true;
+	
+	this.img.onload = function()
 	{
-
-		carregar = true;
-		console.log("carregou" + carregar);
-
+	loaded = true;
+	console.log("carregou fundo" + loaded);
 	}
-
-	this.tamanho_w = 800;
-	this.tamanho_h = 600;
-	this.posicao_x = posicao_x;
-	this.posicao_y = posicao_y;
-
+	
+	this.size_w = 800;
+	this.size_h = 600;
+	this.posicao_x = 0;
+	this.posicao_y = 0;
+	this.velocity_x = 5;
+    this.velocity_y = 5; 
 	this.draw = function()
 	{
-
-	screen.drawImage(this.imagem, this.posicao_x, this.posicao_y)
-
-}
-
-}
+	
+		screen.drawImage(this.img, this.posicao_x, this.posicao_y)
+		
+	}
+	
+ this.keepInScreen=function()
+    {
+    	//saida por cima
+    	if(this.posicao_ < 0)
+    	{
+    		this.posicao_x = 0;
+    	}
+    	
+    }
+	
+}//fecha fundo
