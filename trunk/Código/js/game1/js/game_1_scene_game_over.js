30c44c59479cc5539a625568e47f3bc1d3293fa8
raquelmelorj8@gmail.com <raquelmelorj8@gmail.com@59fae9b3-b18f-dced-0e73-08d28e2dbc6d>
2013-05-28 01:24:56 +0000

    Imagem de vencer e game over no point and click.

--- trunk/Código/js/game1/js/game_1_scene_game_over.js
+++ trunk/Código/js/game1/js/game_1_scene_game_over.js
@@ -3,7 +3,7 @@ function Game1SceneGameOver()
 {
 	//abre Intro
         //file, size_x, size_y, pos_x, pos_y
-		this.fundo = new Fundo("imgs/game_3/game_over.png", 600, 800, 0, 0);
+		this.fundo = new Fundo("imgs/game_1/point.png", 600, 800, 0, 0);
 		this.button_start = new MyButton("imgs/play1.png", 166, 72, 575, 350);
 		this.button_back = new MyButton("imgs/play2.png",166, 72, 575, 450);
 
